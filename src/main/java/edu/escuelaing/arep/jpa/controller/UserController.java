3d3ff3d8c9efd6ed7b993d48dd82e06710a6da9f
juli <bricenojuliana@gmail.com>
2024-10-16 21:26:09 -0500

    encriptando con codec

--- src/main/java/edu/escuelaing/arep/jpa/controller/UserController.java
+++ src/main/java/edu/escuelaing/arep/jpa/controller/UserController.java
@@ -2,10 +2,10 @@ package edu.escuelaing.arep.jpa.controller;
 
 import edu.escuelaing.arep.jpa.entities.User;
 import edu.escuelaing.arep.jpa.repositories.UserRepository;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +18,6 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    // BCryptPasswordEncoder to hash passwords
-    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-
     @PostMapping("/register")
     public ResponseEntity<String> registrarUsuario(@RequestBody User user) {
         // Verifica si el nombre de usuario ya existe
@@ -28,8 +25,8 @@ public class UserController {
             return ResponseEntity.badRequest().body("El nombre de usuario ya existe");
         }
 
-        // Cifra la contraseña con bcrypt
-        String hashedPassword = passwordEncoder.encode(user.getPasswordHash());
+        // Cifra la contraseña con SHA-256
+        String hashedPassword = DigestUtils.sha256Hex(user.getPasswordHash());
         user.setPasswordHash(hashedPassword);
 
         // Guarda el nuevo usuario en la base de datos
@@ -43,8 +40,11 @@ public class UserController {
         User usuarioExistente = userRepository.findByUsername(user.getUsername());
 
         if (usuarioExistente != null) {
-            // Verifica la contraseña proporcionada con bcrypt
-            if (passwordEncoder.matches(user.getPasswordHash(), usuarioExistente.getPasswordHash())) {
+            // Cifra la contraseña proporcionada con SHA-256
+            String hashedPassword = DigestUtils.sha256Hex(user.getPasswordHash());
+
+            // Verifica la contraseña cifrada
+            if (hashedPassword.equals(usuarioExistente.getPasswordHash())) {
                 return ResponseEntity.ok("Inicio de sesión exitoso");
             }
         }
